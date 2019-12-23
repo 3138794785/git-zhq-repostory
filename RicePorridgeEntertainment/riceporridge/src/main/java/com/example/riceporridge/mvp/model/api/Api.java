1fb6806e390e7cb31f369841b389945d504f6628
3138794785 <3138794785@qq.com>
2019-12-23 11:41:41 +0800

    “12/23上传项目
    git commit -m “12/23上传项目

---
+++ RicePorridgeEntertainment/riceporridge/src/main/java/com/example/riceporridge/mvp/model/api/Api.java
@@ -0,0 +1,14 @@
+package com.example.riceporridge.mvp.model.api;
+
+/**
+ * ================================================
+ * 存放一些与 API 有关的东西,如请求地址,请求码等
+ * <p>
+ * Created by MVPArmsTemplate on 12/23/2019 11:32
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * ================================================
+ */
+public interface Api {
+    String APP_DOMAIN = "https://api.github.com";
+}
